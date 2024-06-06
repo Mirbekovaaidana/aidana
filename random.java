0cf37eb50c0b573eb395ad6f231cb55678d43779
Mirbekovaaidana <mirbekovaaidana05@gmail.com>
2024-06-06 16:16:01 +0300

    Add files via upload

---
+++ random.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class random {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("Максимальный элемент в массиве: " + max);
+    }
+}
