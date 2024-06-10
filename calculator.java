be6aaf69252629b688ebc37f9a86ac4d61f21b14
Mirbekovaaidana <mirbekovaaidana05@gmail.com>
2024-06-10 21:36:49 +0300

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Добро пожаловать в калькулятор!");
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Выберите операцию (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Ошибка: деление на ноль!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Неверная операция!");
+                return;
+        }
+
+        System.out.println("Результат операции: " + result);
+    }
+}
